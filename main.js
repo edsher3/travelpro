43f2aeb495aec55aa73c46f22c1d13717dab84c8
edsher3 <eduard.sherstyannikov@gmail.com>
2025-06-05 05:00:49 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+const menuBtn = document.querySelector('.menu__btn');
+
+const menu = document.querySelector ('.menu__list');
+
+menuBtn.addEventListener('click', ()=>{
+    menu.classList.toggle('menu--active');
+})
+
+
+
